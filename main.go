c9fdcad314a5dea944b51725a4880ef3fefe80e2
gretor0231 <gretor0231@gmail.com>
2024-03-08 16:50:07 +0800

    main: added question two and test function.

--- main.go
+++ main.go
@@ -2,10 +2,26 @@ package main
 
 import "fmt"
 
+// print hello world
 func one() string {
 	return "hello world"
 }
 
+//sort the array in ascending order
+func two(input []int) string {
+	//sort the array in ascending order
+	for i := 0; i < len(input); i++ {
+		for j := i + 1; j < len(input); j++ {
+			if input[i] > input[j] {
+				input[i], input[j] = input[j], input[i]
+			}
+		}
+	}
+	//return the sorted array as a string
+	return fmt.Sprint(input)
+}
+
 func main() {
 	fmt.Println(one())
+	fmt.Println(two([]int{10, 3, 4, 2, 7}))
 }
--- main_test.go
+++ main_test.go
@@ -9,3 +9,9 @@ func TestOne(t *testing.T) {
 	res := one()
 	assert.Equal(t, res, "hello world")
 }
+
+func TestTwo(t *testing.T) {
+
+	res := two([]int{10, 3, 4, 2, 7})
+	assert.Equal(t, res, "[2 3 4 7 10]")
+}
