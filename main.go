d8da1d47506c0644356b34f210f66152e4c6c3d6
gretor0231 <gretor0231@gmail.com>
2024-03-08 17:00:55 +0800

    main: added const vars.

--- main.go
+++ main.go
@@ -1,6 +1,72 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
+
+type CardValue int
+
+const (
+	// Diamond
+	TwoDiamond CardValue = iota + 0x102
+	ThreeDiamond
+	FourDiamond
+	FiveDiamond
+	SixDiamond
+	SevenDiamond
+	EightDiamond
+	NineDiamond
+	TenDiamond
+	JackDiamond
+	QueenDiamond
+	KingDiamond
+	AceDiamond
+
+	// Club
+	TwoClub CardValue = iota + 0x202
+	ThreeClub
+	FourClub
+	FiveClub
+	SixClub
+	SevenClub
+	EightClub
+	NineClub
+	TenClub
+	JackClub
+	QueenClub
+	KingClub
+	AceClub
+
+	// Heart
+	TwoHeart CardValue = iota + 0x302
+	ThreeHeart
+	FourHeart
+	FiveHeart
+	SixHeart
+	SevenHeart
+	EightHeart
+	NineHeart
+	TenHeart
+	JackHeart
+	QueenHeart
+	KingHeart
+	AceHeart
+
+	// Spade
+	TwoSpade CardValue = iota + 0x402
+	ThreeSpade
+	FourSpade
+	FiveSpade
+	SixSpade
+	SevenSpade
+	EightSpade
+	NineSpade
+	TenSpade
+	JackSpade
+	QueenSpade
+	KingSpade
+	AceSpade
+)
 
 // print hello world
 func one() string {
@@ -24,4 +90,9 @@ func two(input []int) string {
 func main() {
 	fmt.Println(one())
 	fmt.Println(two([]int{10, 3, 4, 2, 7}))
+
+	fmt.Println(AceClub, TwoDiamond, ThreeDiamond, FourDiamond, FiveDiamond)
+	fmt.Println(TwoClub, ThreeClub, FourClub, FiveClub)
+	fmt.Println(TwoHeart, ThreeHeart, FourHeart, FiveHeart)
+	fmt.Println(TwoSpade, ThreeSpade, FourSpade, FiveSpade)
 }
