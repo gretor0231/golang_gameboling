33fca50c296cf875a7fb0eda833a9c18ec6d4181
gretor0231 <gretor0231@gmail.com>
2024-03-11 15:12:09 +0800

    main: added three functions test cases.

--- functions/three_test.go
+++ functions/three_test.go
@@ -1,1 +1,12 @@
 package functions
+
+import (
+	"github.com/stretchr/testify/assert"
+	"golang_gameboling/enum"
+	"testing"
+)
+
+func TestThree(t *testing.T) {
+	res := three([]int{enum.AceClub, enum.AceHeart, enum.AceDiamond, enum.AceSpade, enum.TenClub})
+	assert.Equal(t, res, enum.Result(3))
+}
